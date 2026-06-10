f77ced2608e61891626135f34c3fc538e7a49086
Sameer gurung <gurungsameer050@gmail.com>
2026-06-10 20:48:04 +0545

    this a simple calculator,made using function ,passing parameter a and b

---
+++ function_OOP/Q1_Calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a//b
+a = int(input("Enter first numbers"))
+b = int(input("Enter second numbers"))
+print(f"sum is {add(a,b)}")
+print(f"subtraction is {sub(a,b)}")
+print(f"multiplication is {multiply(a,b)}")
+print(f"divide is {divide(a,b)}")
+
+
+
